0028e34b81b0fdab02e61809a91d84179d7174c3
BrianDYKim <18k7102dy@naver.com>
2022-12-19 23:23:58 +0900

    프로그래머스 디펜스 게임 not solved

--- src/main/java/org/example/year2022Month12/day19/Programmers12934.java
+++ src/main/java/org/example/year2022Month12/day19/Programmers12934.java
@@ -1,10 +1,18 @@
 package org.example.year2022Month12.day19;
 
 /**
- * @author Doyeop Kim
- * @version 0.0
+ * 정수제곱근 판별
+ * @see <a href="https://school.programmers.co.kr/learn/courses/30/lessons/12934?language=java">...</a>
  * @since 2022/12/19
  */
 public class Programmers12934 {
-    
+    public long solution(long number) {
+        double sqrtNumber = Math.sqrt(number);
+
+        if (sqrtNumber % 1 == 0) {
+            return (long) ((sqrtNumber + 1) * (sqrtNumber + 1));
+        } else {
+            return -1L;
+        }
+    }
 }
---
+++ src/main/java/org/example/year2022Month12/day20/Programmers14208.java
@@ -0,0 +1,15 @@
+package org.example.year2022Month12.day20;
+
+import java.util.*;
+
+/**
+ * @author Doyeop Kim
+ * @version 0.0
+ * @since 2022/12/19
+ */
+public class Programmers14208 {
+
+    public int solution(int n, int k, int[] enemies) {
+        return 0;
+    }
+}
